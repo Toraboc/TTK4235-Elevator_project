79c7038b51d5fdd09da154a4bdb5862c83d62f4d
Anders Holtan <anders.holtan@propulsentnu.no>
2026-01-08 10:09:27 +0100

    mutex to c

--- foo.c
+++ foo.c
@@ -5,20 +5,24 @@
 #include <stdio.h>
 
 int i = 0;
-
+pthread_mutex_t i_lock; // Mutex to protect access to i, Using a mutex because i is shared between threads and not a semaphore because we protect a variable not a resource
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
     for(int j = 0; j < 1000000; j++){
+        pthread_mutex_lock(&i_lock);
         i++;
+        pthread_mutex_unlock(&i_lock);
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
-    for(int j = 0; j < 1000000; j++){
+    for(int j = 0; j < 100000; j++){
+        pthread_mutex_lock(&i_lock);
         i--;
+        pthread_mutex_unlock(&i_lock);
     }   
     return NULL;
 }
@@ -30,6 +34,8 @@ int main(){
     // Hint: search the web! Maybe try "pthread_create example"?
     pthread_t incrementingThread;
     pthread_t decrementingThread;
+
+    pthread_mutex_init(&i_lock, NULL);
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
@@ -38,6 +44,8 @@ int main(){
     // Hint: Use `pthread_join`    
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
+
+    pthread_mutex_destroy(&i_lock);
     
     printf("The magic number is: %d\n", i);
     return 0;
