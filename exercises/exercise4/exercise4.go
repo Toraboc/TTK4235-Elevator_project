611b370cccdf29df8d97880d39cc172fb05f7932
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-02-02 10:17:24 +0100

    chore: Add some unix socket things

--- exercises/exercise4/exercise4.go
+++ exercises/exercise4/exercise4.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "github.com/syucream/posix_mq/src/posix_mq"
+	"net"
 	"os/exec"
 	"runtime"
 	"sync"
@@ -26,7 +26,7 @@ func spawnNewProcess() {
 	os := runtime.GOOS
 	switch os {
 	case "darwin":
-		exec.Command("osascript", "-e", `tell app "Terminal" to do script "go run ` + filename + `"`).Run()
+		exec.Command("osascript", "-e", `tell app "Terminal" to do script "go run `+filename+`"`).Run()
 	case "linux":
 		exec.Command("gnome-terminal", "--", "go", "run", filename).Run()
 	default:
@@ -35,7 +35,65 @@ func spawnNewProcess() {
 }
 
 func backupState() {
-	fmt.Println("Backing up")
+	quit := make(chan bool)
+	lastHB := time.Now()
+	var HBMu sync.Mutex
+
+	go func() {
+		listener, err := net.Listen("unix", "./heartbeat.sock")
+		if err != nil {
+			fmt.Println(err)
+			panic("Failed to listen to file socket")
+		}
+		defer listener.Close()
+		tcpListener := listener.(*net.TCPListener)
+
+		for {
+			select {
+			case <-quit:
+				return
+			default:
+				tcpListener.SetDeadline(time.Now().Add(300 * time.Millisecond))
+				conn, err := tcpListener.Accept()
+				if err != nil {
+					if ne, ok := err.(net.Error); ok && ne.Timeout() {
+						// Accept timed out
+						continue // or break / handle timeout
+					}
+					fmt.Println("Failed to receive a message")
+				}
+
+				buf := make([]byte, 16)
+				nr, err := conn.Read(buf)
+				if err != nil {
+					return
+				}
+
+				data := buf[0:nr]
+				mu.Lock()
+				number = int(data[0])
+				mu.Unlock()
+				
+				HBMu.Lock()
+				lastHB = time.Now()
+				HBMu.Unlock()
+
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(300 * time.Millisecond)
+
+		HBMu.Lock()
+		timeDiff := time.Since(lastHB)
+		HBMu.Unlock()
+
+		if (timeDiff > 300 * time.Millisecond) {
+			quit <- true
+			break
+		}
+	}
 }
 
 func mainState() {
--- exercises/exercise4/go.mod
+++ exercises/exercise4/go.mod
@@ -2,4 +2,3 @@ module exercise4
 
 go 1.24.0
 
-require github.com/syucream/posix_mq v0.0.1
