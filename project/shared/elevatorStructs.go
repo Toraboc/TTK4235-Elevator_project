bbc6e903fd3e2eab8e72b681e89af87d89124bf6
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-02-12 09:28:04 +0100

    chore: Add some helper to find the satte

--- project/shared/elevatorStructs.go
+++ project/shared/elevatorStructs.go
@@ -12,15 +12,15 @@ const (
     MOVING
     PASSENGER_TRANSFER
     DOOR_OBSTRUCTED
-	FAULTY_MOTOR
+    FAULTY_MOTOR
 )
 
 func (behaviour ElevatorBehaviour) CanBeAssignedOrders() bool {
-	return behaviour == IDLE || behaviour == MOVING || behaviour == PASSENGER_TRANSFER
+    return behaviour == IDLE || behaviour == MOVING || behaviour == PASSENGER_TRANSFER
 }
 
 func (behaviour ElevatorBehaviour) CanMove() bool {
-	return behaviour == MOVING || behaviour == FAULTY_MOTOR
+    return behaviour == MOVING || behaviour == FAULTY_MOTOR
 }
 
 type ElevatorState struct {
