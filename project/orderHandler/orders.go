fe6f5947418f2c69426dc6695c0145a4468a0dbd
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-02-26 18:48:27 +0100

    feat(orderHandler): Change the function to get the next target floor

--- project/orderHandler/orders.go
+++ project/orderHandler/orders.go
@@ -62,7 +62,9 @@ func (worldView *WorldView) getNextTargetFloor() (int, error) {
 		return -1, fmt.Errorf("worldView is nil")
 	}
 
-	elevatorState, exists := worldView.ElevatorStates[GetMyId()]
+	myId := GetMyId()
+
+	elevatorState, exists := worldView.ElevatorStates[myId]
 	if !exists {
 		return -1, fmt.Errorf("missing elevator elevatorState for own node")
 	}
@@ -72,129 +74,54 @@ func (worldView *WorldView) getNextTargetFloor() (int, error) {
 		return -1, fmt.Errorf("invalid current floor: %d", floor)
 	}
 
-	
+	myOrders := worldView.Orders[myId]
 
 	if elevatorState.Direction == UP {
 		if elevatorState.Behaviour == PASSENGER_TRANSFER || elevatorState.Behaviour == IDLE {
-			if hasUpRequestAtFloor(worldView, floor) {
+			if myOrders.CabOrders[myId][floor] == CONFIRMED || myOrders.HallUpOrders[floor] == CONFIRMED {
 				return floor, nil
 			}
-			if elevatorState.Behaviour == IDLE {
-				if hasDownRequestAtFloor(worldView, floor) {
-					return floor, nil
-				}
+		}
+
+		for i := floor + 1; i < NumberOfFloors; i++ {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallUpOrders[i] == CONFIRMED {
+				return i, nil
 			}
 		}
-		if target := nearestUpRequestAbove(worldView, floor); target != -1 {
-			return target, nil
+		for i := NumberOfFloors - 1; i >= 0; i-- {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallDownOrders[i] == CONFIRMED {
+				return i, nil
+			}
 		}
-		if target := nearestDownRequestAbove(worldView, floor); target != -1 {
-			return target, nil
+		for i := 0; i <= floor; i++ {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallUpOrders[i] == CONFIRMED {
+				return i, nil
+			}
 		}
-		if hasDownRequestAtFloor(worldView, floor) {
-			return floor, nil
-		}
-		if target := nearestDownRequestBelow(worldView, floor); target != -1 {
-			return target, nil
-		}
-		if target := nearestUpRequestBelow(worldView, floor); target != -1 {
-			return target, nil
-		}
-		if hasUpRequestAtFloor(worldView, floor) {
-			return floor, nil
-		}
-		return -1, nil
 	}
 	//Denne er vel strengt talt ikke nødvendig, men grei for ryddighetens skyld
 	if elevatorState.Direction == DOWN{
 		if elevatorState.Behaviour == PASSENGER_TRANSFER || elevatorState.Behaviour == IDLE {
-			if hasDownRequestAtFloor(worldView, floor) {
+			if myOrders.CabOrders[myId][floor] == CONFIRMED || myOrders.HallDownOrders[floor] == CONFIRMED {
 				return floor, nil
 			}
-			if elevatorState.Behaviour == IDLE {
-				if hasUpRequestAtFloor(worldView, floor) {
-					return floor, nil
-				}
+		}
+
+		for i := floor - 1; i >= 0; i-- {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallDownOrders[i] == CONFIRMED {
+				return i, nil
 			}
 		}
-		if target := nearestDownRequestBelow(worldView, floor); target != -1 {
-			return target, nil
+		for i := range NumberOfFloors {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallUpOrders[i] == CONFIRMED {
+				return i, nil
+			}
 		}
-		if target := nearestUpRequestBelow(worldView, floor); target != -1 {
-			return target, nil
+		for i := NumberOfFloors - 1; i >= floor; i-- {
+			if myOrders.CabOrders[myId][i] == CONFIRMED || myOrders.HallDownOrders[i] == CONFIRMED {
+				return i, nil
+			}
 		}
-		if hasUpRequestAtFloor(worldView, floor) {
-			return floor, nil
-		}
-		if target := nearestUpRequestAbove(worldView, floor); target != -1 {
-			return target, nil
-		}
-		if target := nearestDownRequestAbove(worldView, floor); target != -1 {
-			return target, nil
-		}
-		if hasUpRequestAtFloor(worldView, floor) {
-			return floor, nil
-		}
-		return -1, nil
 	}
 	return -1, nil
-}
-
-
-func hasUpRequestAtFloor(worldView *WorldView, floor int) bool {
-	return worldView.AssignedHallUpOrders[floor] || worldView.AssignedCabOrders[floor]
-}
-
-func hasDownRequestAtFloor(worldView *WorldView, floor int) bool {
-	return worldView.AssignedHallDownOrders[floor] || worldView.AssignedCabOrders[floor]
-}
-func hasRequestAtFloor(worldView *WorldView, floor int) bool {
-	return hasDownRequestAtFloor(worldView, floor) || hasUpRequestAtFloor(worldView, floor)
-}
-
-func nearestUpRequestAbove(worldView *WorldView, floor int) int {
-	if floor == NumberOfFloors -1{//Kunne vel vært -2 her
-		return -1
-	}
-	for f := floor + 1; f < NumberOfFloors; f++ {
-		if hasUpRequestAtFloor(worldView, f) {
-			return f
-		}
-	}
-	return -1
-}
-func nearestDownRequestAbove(worldView *WorldView, floor int) int {
-	if floor == NumberOfFloors -1{
-		return -1
-	}
-	for f := floor + 1; f < NumberOfFloors; f++ {
-		if hasDownRequestAtFloor(worldView, f) {
-			return f
-		}
-	}
-	return -1
-}
-
-func nearestDownRequestBelow(worldView *WorldView, floor int) int {
-	if floor == 0{//Kunne vært 1 istedet for 0 her
-		return -1
-	}
-	for f := floor - 1; f >= 0; f-- {
-		if hasDownRequestAtFloor(worldView, f) {
-			return f
-		}
-	}
-	return -1
-}
-
-func nearestUpRequestBelow(worldView *WorldView, floor int) int {
-	if floor == 0{
-		return -1
-	}
-	for f := floor - 1; f >= 0; f-- {
-		if hasUpRequestAtFloor(worldView, f){
-			return f
-		}
-	}
-	return -1
 }
