54c1a1f6dc4a48f8c115f5bbc0a8d4fc61741a6f
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-03-14 15:16:41 +0100

    chore(network): Update config

--- project/network/config.go
+++ project/network/config.go
@@ -9,10 +9,8 @@ const (
 	BroadcastAddress = "255.255.255.255"
 	// broadcast frequency in Hz.
 	SendHz = 100
-	// frequency for peer list logging.
-	PrintHz = 2
 	// frequency for pruning stale peers.
-	PruneHz = 100
+	PruneHz = 10
 	// duration after which a peer is considered stale.
 	StaleThreshold = 200 * time.Millisecond
 )
