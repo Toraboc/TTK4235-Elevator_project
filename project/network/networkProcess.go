a4906571bbff15ab2fc17cb2c6858e2bbb1b943a
Anders Holtan <55781453+AnhoRhino@users.noreply.github.com>
2026-03-14 14:23:11 +0100

    un go routined function

--- project/network/networkProcess.go
+++ project/network/networkProcess.go
@@ -16,7 +16,7 @@ func NetworkProcess(channels OrderChannels) {
 	knownNodes := newKnownNodes()
 	nodesAwareOfMe := newNodesAwareOfMe()
 
-	go nodeUpdate(knownNodes, nodesAwareOfMe, channels.ConnectedNodesUpdateCh)
+	nodeUpdate(knownNodes, nodesAwareOfMe, channels.ConnectedNodesUpdateCh)
 	go pruneNodes(knownNodes, nodesAwareOfMe, channels.ConnectedNodesUpdateCh)
 	go udpListen(knownNodes, nodesAwareOfMe, channels.ConnectedNodesUpdateCh, channels.WorldViewMergeCh)
 	udpBroadcast(knownNodes, channels.WorldViewReqCh)
